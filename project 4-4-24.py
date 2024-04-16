7c0c34447c9a87bf750c5addb6a5223685f21c50
JustinDenison <justin@denison.org>
2024-04-16 11:30:48 -0500

    Update project 4-4-24.py

--- project 4-4-24.py
+++ project 4-4-24.py
@@ -1,4 +1,14 @@
-import RPi.GPIO as GPIO
+
+###############################################
+# Name: Justin Denison, Pete Schellingerhout, John Auman
+# Date: 3/30/24 (final)
+# A game with many rooms and a puzzle
+# Instructions:
+#   1. take key and go to room 4 to interact with chest
+#   2. go to room 3 to interact with statue
+#   3. go to room 2 to inteact with fireplace
+#################################################
+
 import pygame
 from tkinter import *
 import json
@@ -8,14 +18,17 @@ pygame.mixer.init()
 pygame.mixer.music.load('PsycWard.mp3')
 pygame.mixer.music.play(-1)
 
+# Class representing a room in the game
 class Room(object):
     def __init__(self, name, image):
+        # Initialize room attributes
         self._name = name
         self._image = image
         self._exits = {}
         self._items = {}
-        self._grabbables = {}
+        self._grabbables = []
 
+    # Getter and setter methods for room attributes
     @property
     def name(self):
         return self._name
@@ -56,18 +69,23 @@ class Room(object):
     def grabbables(self, value):
         self._grabbables = value
 
+    # Method to add an exit to the room
     def addExit(self, exit, room):
         self._exits[exit] = room
 
+    # Method to add an item to the room
     def addItem(self, item, desc):
         self._items[item] = desc
 
+    # Method to add a grabbable item to the room
     def addGrabbable(self, item):
         self._grabbables.append(item)
 
+    # Method to remove a grabbable item from the room
     def delGrabbable(self, item):
         self._grabbables.remove(item)
 
+    # Method to return a string description of the room
     def __str__(self):
         s = "You are in {}.\n".format(self.name)
         s += "You see: "
@@ -84,10 +102,13 @@ class Room(object):
         return s
 
 
+# Class representing the game
 class Game(Frame):
     def __init__(self, parent):
         Frame.__init__(self, parent)
 
+    # Method to create rooms in the game
+    # Method to create rooms in the game
     def createRooms(self):
         with open('rooms.json') as f:
             data = json.load(f)
@@ -99,22 +120,25 @@ class Game(Frame):
             room = Room(room_data['name'], room_data['image'])
             room.exits = room_data.get('exits', {})
             room.items = room_data.get('items', {})
-            room.grabbables = room_data.get('grabbables', {})
+            room.grabbables = room_data.get('grabbables', [])
 
+            # Check if there's a diary item, and if so, read its content from the file
             for item, desc in room.items.items():
                 if item.startswith("diary"):
                     diary_number = item.split("diary")[1]
                     with open(f"diary{diary_number}.txt", "r") as f:
                         diary_content = f.read()
-                    room.items[item] = diary_content.strip()
+                    room.items[item] = diary_content.strip()  # Set the content of the diary
 
             rooms[room.name] = room
 
+        # Link exits
         for room_data in rooms_data:
             room = rooms[room_data['name']]
             for exit_dir, exit_room_name in room_data.get('exits', {}).items():
                 room.addExit(exit_dir, rooms[exit_room_name])
 
+        # Set initial room
         Game.currentRoom = rooms['Room 1']
         Game.inventory = []
 
@@ -138,6 +162,7 @@ class Game(Frame):
         text_frame.pack(side=RIGHT, fill=Y)
         text_frame.pack_propagate(False)
 
+    # Method to set the current room image
     def setRoomImage(self):
         if (Game.currentRoom == None):
             Game.img = PhotoImage(file="skull.gif")
@@ -147,6 +172,7 @@ class Game(Frame):
         Game.image.config(image=Game.img)
         Game.image.image = Game.img
 
+    # Method to set the status displayed on the right of the GUI
     def setStatus(self, status):
         Game.text.config(state=NORMAL)
         Game.text.delete("1.0", END)
@@ -158,23 +184,14 @@ class Game(Frame):
                 Game.inventory) + "\n\n" + status + "\n\n\n" + possible_actions)
             Game.text.config(state=DISABLED)
 
+    # Method to start playing the game
     def play(self):
         self.createRooms()
         self.setupGUI()
         self.setRoomImage()
         self.setStatus("")
 
-        while True:
-            buttonstate1 = GPIO.input(25)
-            buttonstate2 = GPIO.input(24)
-            buttonstate3 = GPIO.input(23)
-            buttonstate4 = GPIO.input(26)
-
-            if not buttonstate1 or not buttonstate2 or not buttonstate3 or not buttonstate4:
-                handleButton(buttonstate1, buttonstate2, buttonstate3, buttonstate4)
-            else:
-                window.update()
-
+    # Method to process the player's input
     def process(self, event):
         action = Game.player_input.get()
         action = action.lower()
@@ -202,8 +219,10 @@ class Game(Frame):
             if (noun in Game.currentRoom.items):
                 response = Game.currentRoom.items[noun]
         elif verb == "interact":
+            # Check if the noun is interactable
             if noun in ["chair", "table", "rug", "brewrig"]:
                 response = Game.currentRoom.items[noun]
+            # Handle specific interactions based on items and inventory
             elif noun == "chest":
                 if "key" in Game.inventory and "hammer" not in Game.inventory:
                     response = "Interesting! The key unlocked the chest and in it you found a hammer!"
@@ -235,38 +254,20 @@ class Game(Frame):
         Game.player_input.delete(0, END)
 
 
-def handleButton(buttonstate1, buttonstate2, buttonstate3, buttonstate4):
-    action = ""
-    if not buttonstate1:
-        action = "go north"
-    elif not buttonstate2:
-        action = "go east"
-    elif not buttonstate3:
-        action = "go south"
-    elif not buttonstate4:
-        action = "go west"
+######################MAIN#########################
 
-    g.process(action)
-
-
+# Default size of the GUI
 WIDTH = 1600
 HEIGHT = 1200
 
+# Create the window
 window = Tk()
 window.title("Room Adventure")
 
-North = 25
-East = 24
-South = 23
-West = 26
-
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(25, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(26, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-
+# Create the GUI as a Tkinter canvas inside the window
 g = Game(window)
+# Play the game
 g.play()
 
+# Wait for the window to close
 window.mainloop()
